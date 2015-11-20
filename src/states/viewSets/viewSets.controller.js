d6084b8e1ac377b3b5d60d875cfc5599c3005ee3
Todd Kronenberg <toddkronenberg@gmail.com>
2015-11-19 18:45:24 -0600

    Disabled link to view cards page if no cards. Also disabled next button in view cards page if only 1 card in set.

--- src/states/viewSets/viewSets.controller.js
+++ src/states/viewSets/viewSets.controller.js
@@ -13,5 +13,12 @@ function ViewSetsCtrl($state, Flashcard) {
 
   vmViewSets.stateName = $state.current.name;
   vmViewSets.sets = Flashcard.get();
+  vmViewSets.goToCards = goToCards;
+
+
+  function goToCards(setTitle) {
+    if (vmViewSets.sets[setTitle].cards.length)
+      $state.go('viewCards', {setTitle: setTitle});
+  }
 
 }
